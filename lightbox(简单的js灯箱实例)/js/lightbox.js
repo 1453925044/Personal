00a8c1d484a9564e657d7fbbd8c1f20d69dbeb4a
Mr.Wei <1453925044@qq.com>
2018-04-11 08:44:27 +0800

    小修改

--- lightbox(简单的js灯箱实例)/js/lightbox.js
+++ lightbox(简单的js灯箱实例)/js/lightbox.js
@@ -1,41 +1,40 @@
 var img_index = 0;
 var img_src = "";
 $(function () {
-   var lightbox=(function(){
-          // 点击关闭弹出框
-    $(".photo-close").on("click", function () {
-        $(".pop").hide();
-        $(".shadow").hide();
-    })
-    //下一张图片
-    $(".pop .photo-div .arrow-next").click(function () {
-        img_index++;
-        if (img_index >= $(".show li img").length) {
-            img_index = 0;
-        }
-        img_src = $(".show li img").eq(img_index).attr("src");
-        photoView($(".show li img"));
-    });
-    //上一张图片
-    $(".pop .photo-div .arrow-prv").click(function () {
-        img_index--;
-        if (img_index < 0) {
-            img_index = $(".show li img").length - 1;
-        }
-        img_src = $(".show li img").eq(img_index).attr("src");
-        photoView($(".show li img"));
-    });
-    //弹出框图片显示
-    $(".show li img").click(function () {
-        $(".shadow").show();
-        $(".pop").show();
-        img_src = $(this).attr("src");
-        img_index = $(this).index();
-        photoView($(this));
-    });
-    return window.lightbox()
+    var lightbox = (function () {
+        // 点击关闭弹出框
+        $(".photo-close").on("click", function () {
+            $(".pop").hide();
+            $(".shadow").hide();
+        })
+        //下一张图片
+        $(".pop .photo-div .arrow-next").click(function () {
+            img_index++;
+            if (img_index >= $(".show li img").length) {
+                img_index = 0;
+            }
+            img_src = $(".show li img").eq(img_index).attr("src");
+            photoView($(".show li img"));
+        });
+        //上一张图片
+        $(".pop .photo-div .arrow-prv").click(function () {
+            img_index--;
+            if (img_index < 0) {
+                img_index = $(".show li img").length - 1;
+            }
+            img_src = $(".show li img").eq(img_index).attr("src");
+            photoView($(".show li img"));
+        });
+        //弹出框图片显示
+        $(".show li img").click(function () {
+            $(".shadow").show();
+            $(".pop").show();
+            img_src = $(this).attr("src");
+            img_index = $(this).index();
+            photoView($(this));
+        });
+        return window.lightbox()
     })()
-  
 });
 
 //自适应预览
